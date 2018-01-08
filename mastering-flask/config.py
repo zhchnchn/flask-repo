7a03900fcaa9e64418cbf7f690505aa2634f0cce
zhchnchn <zjl_133@163.com>
2018-01-08 15:07:42 +0800

    ch02: add Flask-SQLAlchemy extension

--- mastering-flask/config.py
+++ mastering-flask/config.py
@@ -1,3 +1,8 @@
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
@@ -10,7 +15,6 @@ class ProductConfig(Config):
 
 class DevConfig(Config):
     DEBUG = True
-
-
-
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    SQLALCHEMY_COMMIT_ON_TERDOWN = True
 
--- mastering-flask/main.py
+++ mastering-flask/main.py
@@ -1,10 +1,14 @@
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 from config import DevConfig
 
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
+
+db = SQLAlchemy(app)
 
 
 @app.route("/")
