d697cb6191854baec319aa88a93328956c581013
zhchnchn <zjl_133@163.com>
2018-01-08 17:32:43 +0800

    ch02: add model: Post

--- mastering-flask/main.py
+++ mastering-flask/main.py
@@ -17,12 +17,29 @@ class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(255))
     password = db.Column(db.String(255))
+    posts = db.relationship('Post', backref='user', lazy='dynamic')
 
     def __init__(self, username):
         self.username = username
 
     def __repr__(self):
         return "<User, '{}'>".format(self.username)
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    publish_date = db.Column(db.DateTime())
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id'))
+
+    def __init__(self, title):
+        self.title = title
+
+    def __repr__(self):
+        return "<Post '{}'>".format(self.title)
 
 
 @app.route("/")
