9af0260fc5033a0ae20ac0d8599f15b5b39afa8a
zhchnchn <zjl_133@163.com>
2018-01-08 17:40:01 +0800

    ch02: add model: Comment

--- mastering-flask/main.py
+++ mastering-flask/main.py
@@ -34,12 +34,26 @@ class Post(db.Model):
     text = db.Column(db.Text())
     publish_date = db.Column(db.DateTime())
     user_id = db.Column(db.Integer(), db.ForeignKey('users.id'))
+    comments = db.relationship('Comment', backref='post', lazy='dynamic')
 
     def __init__(self, title):
         self.title = title
 
     def __repr__(self):
         return "<Post '{}'>".format(self.title)
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    date = db.Column(db.DateTime())
+    post_id = db.Column(db.Integer(), db.ForeignKey('posts.id'))
+
+    def __repr__(self):
+        return "<Comment '{}'>".format(self.text[:15])
 
 
 @app.route("/")
