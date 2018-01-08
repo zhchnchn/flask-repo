3e589731bb506cc09db913e1a74fe7a2c48868ce
zhchnchn <zjl_133@163.com>
2018-01-08 18:06:45 +0800

    ch02: add model: Tag, tags_table and n-n relationship for Post & Tag

--- mastering-flask/main.py
+++ mastering-flask/main.py
@@ -35,6 +35,8 @@ class Post(db.Model):
     publish_date = db.Column(db.DateTime())
     user_id = db.Column(db.Integer(), db.ForeignKey('users.id'))
     comments = db.relationship('Comment', backref='post', lazy='dynamic')
+    tags = db.relationship('Tag', secondary=tags_table,
+                           backref=db.backref('posts', lazy='dynamic'))
 
     def __init__(self, title):
         self.title = title
@@ -54,6 +56,27 @@ class Comment(db.Model):
 
     def __repr__(self):
         return "<Comment '{}'>".format(self.text[:15])
+
+
+class Tag(db.Model):
+    __tablename__ = 'tags'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255))
+
+    def __init__(self, title):
+        self.title = title
+
+    def __repr__(self):
+        return "<Tag '{}'>".format(self.title)
+
+
+# tags_table variable refer to the 'posts_tags' Table
+tags_table = db.Table(
+    'posts_tags',
+    db.Column('post_id', db.Integer(), db.ForeignKey('posts.id')),
+    db.Column('tag_id', db.Integer(), db.ForeignKey('tags.id'))
+)
 
 
 @app.route("/")
