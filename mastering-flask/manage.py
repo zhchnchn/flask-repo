1bd03c5f85a528643dc576b2d564dc918be72bf5
zhchnchn <zjl_133@163.com>
2018-01-09 18:27:21 +0800

    ch03: export Comment class in python shell

--- mastering-flask/manage.py
+++ mastering-flask/manage.py
@@ -3,7 +3,7 @@
 
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
-from main import app, db, User, Post, Tag
+from main import app, db, User, Post, Tag, Comment
 
 migrate = Migrate(app, db)
 manager = Manager(app)
@@ -14,7 +14,7 @@ manager.add_command('db', MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Post=Post, Tag=Tag)
+    return dict(app=app, db=db, User=User, Post=Post, Tag=Tag, Comment=Comment)
 
 
 if __name__ == '__main__':
