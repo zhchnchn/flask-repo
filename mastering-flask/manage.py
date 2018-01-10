d7ed6936f66d65a9117d2b4843928f9ca6f1cdd9
zhchnchn <zjl_133@163.com>
2018-01-10 11:11:36 +0800

    ch05: add Application Factory method: create_app

--- mastering-flask/manage.py
+++ mastering-flask/manage.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
+import os
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
-from webapp import app
+from webapp import create_app
 from webapp.models import db, User, Post, Tag, Comment
+
+
+# 默认使用 Dev 配置
+env = os.environ.get('WEBAPP_ENV', 'Dev')
+app = create_app('webapp.config.{}Config'.format(env.capitalize()))
 
 migrate = Migrate(app, db)
 manager = Manager(app)
--- mastering-flask/webapp/__init__.py
+++ mastering-flask/webapp/__init__.py
@@ -1,20 +1,30 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, redirect, url_for
-from config import DevConfig
+from flask import Flask
 from controllers.blog import blog_blueprint
 from controllers.main import main_blueprint
 from models import db
 
 
-app = Flask(__name__)
-app.config.from_object(DevConfig)
+def create_app(object_name):
+    """
+    An flask application factory, as explained here:
+    http://flask.pocoo.org/docs/patterns/appfactories/
+    Arguments:
+        object_name: the python path of the config object,
+                     e.g. webapp.config.DevConfig
+    """
 
-db.init_app(app)
+    app = Flask(__name__)
+    app.config.from_object(object_name)
+    # init SQLAlchemy db
+    db.init_app(app)
+    # register blueprint
+    app.register_blueprint(blog_blueprint)
+    app.register_blueprint(main_blueprint)
 
-
-app.register_blueprint(blog_blueprint)
-app.register_blueprint(main_blueprint)
+    return app
 
 
 if __name__ == '__main__':
+    app = create_app('webapp.config.DevConfig')
     app.run()
