c3f498708fccc39788b7f91b561a2d40811dc674
zhchnchn <zjl_133@163.com>
2018-01-10 10:50:03 +0800

    ch05: add main_blueprint

--- mastering-flask/webapp/__init__.py
+++ mastering-flask/webapp/__init__.py
@@ -2,6 +2,7 @@
 from flask import Flask, redirect, url_for
 from config import DevConfig
 from controllers.blog import blog_blueprint
+from controllers.main import main_blueprint
 from models import db
 
 
@@ -11,15 +12,8 @@ app.config.from_object(DevConfig)
 db.init_app(app)
 
 
-@app.route('/')
-def index():
-    """由于蓝图添加了URL前缀，因此在基础app对象上已经没有任何视图了，访问网站的根路径，
-    将没有对应的视图函数，因此在根路径上加一个重定向
-    """
-    return redirect(url_for('blog.home'))
-
-
 app.register_blueprint(blog_blueprint)
+app.register_blueprint(main_blueprint)
 
 
 if __name__ == '__main__':
