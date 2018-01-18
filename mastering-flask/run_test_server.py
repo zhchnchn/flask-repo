e8bec6aa38a0a9b26401d3e329513b891aa117a0
zhchnchn <zjl_133@163.com>
2018-01-18 18:28:13 +0800

    ch12: add selenium system test

---
+++ mastering-flask/run_test_server.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+结合tests/test_ui.py使用
+"""
+from webapp import create_app
+from webapp.models import db, User, Role
+
+app = create_app('webapp.config.TestConfig')
+
+# Bug workaround
+db.app = app
+db.create_all()
+
+
+default = Role("default")
+poster = Role("poster")
+db.session.add(default)
+db.session.add(poster)
+db.session.commit()
+
+test_user = User("test")
+test_user.set_password("test")
+test_user.roles.append(poster)
+db.session.add(test_user)
+db.session.commit()
+
+app.run()
