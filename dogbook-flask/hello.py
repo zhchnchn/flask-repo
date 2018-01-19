0430af7dcf053a69efd991e799225ff0257c9f35
zhchnchn <zjl_133@163.com>
2018-01-20 00:36:01 +0800

    dogbook_ch03: add Flask-Bootstrap extension

--- dogbook-flask/hello.py
+++ dogbook-flask/hello.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 from flask_script import Manager, Server
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 manager.add_command('server', Server())
 
