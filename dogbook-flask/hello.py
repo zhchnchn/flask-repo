7ed29a114cb0b32170ccfb982cd8c461837000b7
zhchnchn <zjl_133@163.com>
2018-01-20 00:51:53 +0800

    dogbook_ch03: add base.html template

--- dogbook-flask/hello.py
+++ dogbook-flask/hello.py
@@ -10,6 +10,16 @@ bootstrap = Bootstrap(app)
 manager.add_command('server', Server())
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
