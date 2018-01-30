9bf638b2eb80321b35204062a47e3d75881e3038
zhchnchn <zjl_133@163.com>
2018-01-30 23:30:28 +0800

    ch15: 为test命令添加--coverage参数

--- mastering-flask/manage.py
+++ mastering-flask/manage.py
@@ -8,6 +8,12 @@ from flask_migrate import Migrate, MigrateCommand
 from webapp import create_app
 from webapp.models import db, User, Post, Tag, Comment, Role, Follow
 
+# 保证在全局作用域中的所有代码执行之前，启动覆盖检测
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='webapp/*')
+    COV.start()
 
 # 默认使用 default 配置
 app = create_app(os.environ.get('FLASK_CONFIG', 'default'))
@@ -53,12 +59,32 @@ def clear_data():
     db.session.commit()
 
 
+# test命令添加coverage参数,Flask-Script根据参数名确定选项名，并据此向函数中传入True或False
+# 调用参数的方法： python manage.py test --coverage
 @manager.command
-def test():
+def test(coverage=False):
     """Run the unit tests in the tests/unit folder."""
+    # 如果没有设置环境变量，则先设定环境变量FLASK_COVERAGE，之后，脚本会重启
+    # 再次运行时，脚本顶端的代码发现已经设定了环境变量，于是立即启动覆盖检测。
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        import sys
+        os.environ['FLASK_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
+    # 执行测试
     import unittest
     tests = unittest.TestLoader().discover('tests/unit')
     unittest.TextTestRunner(verbosity=2).run(tests)
+    # 输出覆盖报告
+    if COV:
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print('HTML version: file://%s/index.html' % covdir)
+        COV.erase()
 
 
 if __name__ == '__main__':
